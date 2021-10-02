7d6d873cc0879071ef10f69900d5df87a6c712f6
Francisco Villarroel <frvillarro@fen.uchile.cl>
2021-10-02 15:02:46 -0300

    finished academic section + joined all sections + exported it to the existent .xlsx (in order to rewrite it everytime I run the script).

--- dofile/03_descriptive_01table.R
+++ dofile/03_descriptive_01table.R
@@ -1,7 +1,9 @@
+#' this script generates the descriptive table 01 of the paper.
+
 if(!require("pacman")){install.packages("pacman")}
 
 # load libraries
-pacman::p_load(tidyverse, data.table, janitor, here, haven, magrittr)
+pacman::p_load(tidyverse, data.table, janitor, here, haven, magrittr, openxlsx)
 
 # load data
 data <- read_dta(here("data", "proc", "working_dataset_cohorte4m_2010.dta"))
@@ -29,7 +31,9 @@ data_2 %>% select(starts_with("titulado"), duracion_total_anios) %>%
     mutate(name = paste0(name,"_4a")) %>%
     group_by(name) %>%
     summarise_all(list(mean = mean, sum = sum)) %>% 
-    rbind(NA, NA))
+    rbind(NA, NA))%>%
+    
+    mutate(mean = round(mean, 3))
 
 
 # demographic ----
@@ -62,7 +66,9 @@ data_2 %>% select(d_mujer_alu, d_estudia_otra_region) %>%
     pivot_longer(everything()) %>%
     group_by(name) %>%
     summarise_all(list(mean = mean, sum = sum)) %>%
-    rbind(NA, NA))
+    rbind(NA, NA))%>%
+    
+    mutate(mean = round(mean, 3))
 
 # institutional ----
 
@@ -102,22 +108,55 @@ data_2 %>% group_by(area_conocimiento_cat) %>%
               sum = n()) %>%
     ungroup() %>%
     rename(name = area_conocimiento_cat) %>%
-    rbind(NA))
+    filter(!str_detect(name, "definida$")) %>%
+    rbind(NA)) %>%
+    
+    mutate(mean = round(mean, 3))
 
-institutional_section
+# academic ----
 
-# # academic ----
-# # I will keep this pending.
-# 
-# # these are not working.
-# data_2 %>% select(starts_with("ptje"), nem) %>%
-#     summarise(ptje_simce_lect = mean(ptje_lect2m_alu),
-#               ptje_simce_mat = mean(ptje_mate2m_alu),
-#               nem = mean(nem))
-# 
-# # Need to impute the values of these tests in Stata first..
-# data_2 %$% sum(is.na((ptje_lect2m_alu)))
-# 
+# 1381 NAs.
+#data_2 %$% sum(is.na((ptje_lect2m_alu)))
+#data_2 %$% sum(is.na((ptje_mate2m_alu)))
+
+academic_section <- 
+data_2 %>% select(starts_with("ptje"), nem) %>%
+    pivot_longer(everything()) %>%
+    group_by(name) %>%
+    summarise_all(.funs = list(mean = mean, sum = ~sum(!is.na(.))), na.rm = T) %>%
+    mutate(mean = round(mean,1)) %>%
+    rbind(NA)
+
+
+
+# join all sections ----
+table <- 
+graduation_section %>% bind_rows(demographic_section) %>%
+                        bind_rows(institutional_section) %>%
+                        bind_rows(academic_section) %>%
+                        mutate(mean = mean %>% str_replace("\\.", ","))
+
+
+# overwrite results over existing table
+
+## Load existing file
+excel_file <- loadWorkbook(here("results", "tables", "descriptive","03_descriptive_table01.xlsx"))
+
+## Pull all data from sheet 1
+excel_table = read.xlsx(excel_file, sheet=1)
+
+## join data
+table <- table %>% add_row(mean = NA, .before = 1) # this is just for format!
+excel_table <- excel_table %>% mutate("Valor.(%)" = table %>% pull(mean),
+                                        "N" = table %>% pull(sum))
+
+## Put the data back into the workbook
+writeData(excel_file, sheet=1, excel_table)
+
+## Save to disk
+saveWorkbook(excel_file, 
+             here("results", "tables", "descriptive","03_descriptive_table01.xlsx"), 
+             overwrite = TRUE)
 
 #' asddas
 #' asdadsdsaadsdsa
