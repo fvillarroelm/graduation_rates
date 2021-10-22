8433b5609e178f8989559d6c94867022f9e904c7
Francisco Villarroel <frvillarro@fen.uchile.cl>
2021-10-22 15:27:31 -0300

    formatting changes

--- dofile/03_descriptive_table03_in-time-graduation-rates.R
+++ dofile/03_descriptive_table03_in-time-graduation-rates.R
@@ -1,15 +1,14 @@
-#' this script generates the descriptive table 02 of the paper.
-rm(list = ls())
+#' this script generates the descriptive table 03 of the paper.
 if(!require("pacman")){install.packages("pacman")}
 
 # load libraries
 pacman::p_load(tidyverse, data.table, janitor, here, haven, magrittr, openxlsx, broom)
 
 # load data
-data <- read_dta(here("data", "proc", "working_dataset_cohorte4m_2010.dta"))
+data_raw <- read_dta(here("data", "proc", "working_dataset_cohorte4m_2010.dta"))
 
 data_2 <-
-    data %>% filter(entra_ES == 1 & 
+    data_raw %>% filter(entra_ES == 1 & 
                         area_conocimiento_cat != 10 & 
                         is.na(ptje_lect2m_alu) == 0 &
                         is.na(ptje_mate2m_alu) == 0
@@ -26,8 +25,6 @@ data_2 %>% filter(duracion_total_anios >= 4)
 # table function ----
 
 table <- function(data){
-    
-    #graduation_var <- graduation_var
     
     # row names
     row_names <-
@@ -52,42 +49,50 @@ table <- function(data){
     # numerator/denominator (graduation rate by knowledge area)
     table_grads <- round(grads / grads_and_nongrads, 3) %>% 
         mutate(area_conocimiento_cat = row_names) %>%
-        relocate(area_conocimiento_cat)
+        relocate(area_conocimiento_cat) %>%
+        pivot_longer(2:5, names_to = "school_type", values_to = "value") # to "long" data
     
-    # diff pp - mun for each program.
-    diff_pp_mun <-
+    # diff pp and ps with mun for each program.
+    diff_pp_and_ps_with_mun <-
         map_df(.x = data %$% levels(area_conocimiento_cat),
                .f = ~{data %>% filter(area_conocimiento_cat == .x) %>%
                        lm (titulado_oportuno ~ dependencia_cat, .) %>% 
                        tidy() %>%
-                       tail(1) %>% # diff between pp and m
                        mutate(p.value = round(p.value, 3),
-                              dif_pp_m = round(estimate, 3),
-                              area_conocimiento_cat = .x) %>%
-                       select(area_conocimiento_cat, dif_pp_m, p.value)}) %>%
+                              #diff = round(estimate, 3),
+                              area_conocimiento_cat = .x,
+                              school_type = term %>% str_remove("dependencia_cat")) %>%
+                       filter(!str_detect(school_type, "Intercept")) %>%
+                       select(area_conocimiento_cat, school_type, p.value) # excluded diff
+               }
+        ) %>%
         
         bind_rows(
             # total row!
             data %>% lm (titulado_oportuno ~ dependencia_cat, .) %>% 
                 tidy() %>%
-                tail(1) %>% # diff between pp and m
                 mutate(p.value = round(p.value, 3),
-                       dif_pp_m = round(estimate, 3),
-                       area_conocimiento_cat = "Total") %>%
-                select(area_conocimiento_cat, dif_pp_m, p.value))
-    
+                       #diff = round(estimate, 3),
+                       area_conocimiento_cat = "Total",
+                       school_type = term %>% str_remove("dependencia_cat")) %>%
+                filter(!str_detect(school_type, "Intercept")) %>%
+                select(area_conocimiento_cat, school_type, p.value) #excluded diff
+        )
     
     
     # final table
-    final_table <- table_grads %>% left_join(diff_pp_mun, by="area_conocimiento_cat") %>%
-        mutate(dif_pp_m = case_when(p.value > 0.05 & p.value <= 0.1 ~ paste0(dif_pp_m,"*"),
-                                    p.value > 0.01 & p.value <= 0.05 ~ paste0(dif_pp_m, "**"),
-                                    p.value <= 0.01 ~ paste0(dif_pp_m,"***"),
-                                    TRUE ~ as.character(dif_pp_m))) %>%
-        select(-p.value)
+    final_table <- table_grads %>% left_join(diff_pp_and_ps_with_mun, 
+                                             by = c("area_conocimiento_cat", "school_type")) %>%
+        mutate(value = as.character(value*100) %>% str_replace_all(., "\\.", ",")) %>%
+        mutate(value = case_when(p.value > 0.05 & p.value <= 0.1 ~ paste0(value, "% *"),
+                                 p.value > 0.01 & p.value <= 0.05 ~ paste0(value, "% **"),
+                                 p.value <= 0.01 ~ paste0(value, "% ***"),
+                                 TRUE ~ paste0(value, "%"))) %>%
+        select(-p.value) %>%
+        pivot_wider(names_from = "school_type", values_from = "value")
     
     return(final_table)
-
+    
 }
 
 
@@ -102,7 +107,7 @@ final_table <- table_3y %>% bind_cols(table_4y)
 # export ----
 
 # load existing file
-excel_file <- loadWorkbook(here("results", "tables", "descriptive","03_descriptive_table03.xlsx"))
+excel_file <- loadWorkbook(here("results", "tables", "descriptive","03_descriptive_table03_in-time-grad-rates.xlsx"))
 
 # pull all data from sheet 1
 excel_table <- read.xlsx(excel_file, sheet=1)
@@ -116,5 +121,5 @@ writeData(excel_file, sheet=1, excel_table)
 
 # save to disk
 saveWorkbook(excel_file, 
-             here("results", "tables", "descriptive","03_descriptive_table03.xlsx"), 
+             here("results", "tables", "descriptive","03_descriptive_table03_in-time-grad-rates.xlsx"), 
              overwrite = TRUE)
